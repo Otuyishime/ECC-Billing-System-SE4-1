caec0acd73aac0b33274626ecb79224b6258fe6d
olivier tuyishime <oliviertyishime@gmail.com>
2016-08-14 16:28:26 -0500

    Added UI for maintaining the projects

--- src/ecc_UI/MaintainProjectJPanel.java
+++ src/ecc_UI/MaintainProjectJPanel.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import testECC.*;
 import java.awt.Color;
 import java.awt.Rectangle;
+import java.awt.Font;
 
 public class MaintainProjectJPanel extends JPanel {
 	
@@ -14,6 +15,10 @@ public class MaintainProjectJPanel extends JPanel {
 	public MaintainProjectJPanel(JFrame currentFrame, Employee employee) {
 		setBounds(new Rectangle(0, 0, 900, 700));
 		setBackground(Color.LIGHT_GRAY);
+		
+		JLabel lblNewLabel = new JLabel("Maintain Projects");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
+		add(lblNewLabel);
 				
 		for (Project p : employee.getProjects()){
 			p.print();
